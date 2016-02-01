fa53d1dcd8bf1883068a493cfa0d2f55100e0563
Robert Dempsey <robertonrails@gmail.com>
2016-02-01 10:18:47 -0500

    Update to the main spider
    Signed-off-by: Robert Dempsey <robertonrails@gmail.com>

--- govbenefitsspider/govbenefitsspider/spiders/__init__.py
+++ govbenefitsspider/govbenefitsspider/spiders/__init__.py
@@ -4,22 +4,22 @@
 # your spiders.
 
 import scrapy
-from govbenefitsspider.items import BenefitProgramItem
-from govbenefitsspider.items import BenefitProgramDetail
+# from govbenefitsspider.items import BenefitProgramItem
+# from govbenefitsspider.items import BenefitProgramDetail
 
 
 # Scraper to save the entire response to a file
-# class BenefitProgramSpider(scrapy.Spider):
-#     name = "benefitprograms"
-#     allowed_domains = ["benefits.gov"]
-#     start_urls = [
-#         "http://www.benefits.gov/benefits/browse-by-category/category/FOO",
-#     ]
+class BenefitProgramSpider(scrapy.Spider):
+    name = "benefitprograms"
+    allowed_domains = ["benefits.gov"]
+    start_urls = [
+        "http://www.benefits.gov/benefits/browse-by-category/category/FOO",
+    ]
 
-#     def parse(self, response):
-#         filename = response.url.split("/")[-2]
-#         with open(filename, 'wb') as f:
-#             f.write(response.body)
+    def parse(self, response):
+        filename = response.url.split("/")[-2]
+        with open(filename, 'wb') as f:
+            f.write(response.body)
 
 
 # Scraper to grab only the listing page
@@ -51,24 +51,24 @@ from govbenefitsspider.items import BenefitProgramDetail
 
 
 # Full on looping spider
-class BenefitProgramSpider(scrapy.Spider):
-    name = "benefitprograms"
-    allowed_domains = ["benefits.gov"]
-    start_urls = [
-        "http://www.benefits.gov/benefits/browse-by-category/category/FOO",
-    ]
+# class BenefitProgramSpider(scrapy.Spider):
+#     name = "benefitprograms"
+#     allowed_domains = ["benefits.gov"]
+#     start_urls = [
+#         "http://www.benefits.gov/benefits/browse-by-category/category/FOO",
+#     ]
 
-    def parse(self, response):
-        for href in response.css("span.benefit-header > a::attr('href')"):
-            url = response.urljoin(href.extract())
-            yield scrapy.Request(url, callback=self.parse_dir_contents)
+#     def parse(self, response):
+#         for href in response.css("span.benefit-header > a::attr('href')"):
+#             url = response.urljoin(href.extract())
+#             yield scrapy.Request(url, callback=self.parse_dir_contents)
 
-    def parse_dir_contents(self, response):
-        item = BenefitProgramDetail()
-        item['title'] = response.xpath(
-            '//div[@class="span8 benefit-detail-title"]/text()').extract()
-        item['agency'] = response.xpath(
-            '//div[@class="span4 benefit-detail-agency"]/span[2]/text()').extract()
-        item['state_link'] = response.xpath(
-            '//div[@class="span4 benefit-detail-agency"]/span[3]/a/@href').extract()
-        yield item
+#     def parse_dir_contents(self, response):
+#         item = BenefitProgramDetail()
+#         item['title'] = response.xpath(
+#             '//div[@class="span8 benefit-detail-title"]/text()').extract()
+#         item['agency'] = response.xpath(
+#             '//div[@class="span4 benefit-detail-agency"]/span[2]/text()').extract()
+#         item['state_link'] = response.xpath(
+#             '//div[@class="span4 benefit-detail-agency"]/span[3]/a/@href').extract()
+#         yield item
